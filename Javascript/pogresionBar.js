fd9c4dfbd033b08808188c113552584519c16aec
mahdi2662 <madialkhalidi83@gmail.com>
2024-09-17 13:19:22 +0200

    progres bar javascipt edit

--- Javascript/pogresionBar.js
+++ Javascript/pogresionBar.js
@@ -6,3 +6,21 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("pbar").style.width = scrolled + "%";
 }
+
+
+
+let lastScrollTop = 0;
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrollen naar beneden - verberg de nav
+        nav.style.top = `-${nav.offsetHeight}px`; // Verbergt de nav met een animatie
+    } else {
+        // Scrollen naar boven - toon de nav
+        nav.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+});
