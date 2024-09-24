2e63d21b6c10383fffd400b0ce06b030ab543d30
mahdi2662 <madialkhalidi83@gmail.com>
2024-09-24 13:35:29 +0200

    stand erbij gedaan

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 define("ROOT", __DIR__ . "/");
-define("URL", "http://projectsne.com");
+define("URL", "http://sneakerness.com");
---
+++ pages/stand.php
@@ -0,0 +1,19 @@
+<?php include_once "../includes/header.php" ?>
+<!-- All extra links for styling etc.-->
+<title>Sneakerness - </title>
+</head>
+
+<body>
+    <?php include_once "../includes/navbar.php" ?>
+
+ 
+    <h2></h2>
+    <h3></h3>
+    <p></p>
+
+    <?php include_once "../includes/footer.php" ?>
+
+    <!-- All extra scripts -->
+</body>
+
+</html>
