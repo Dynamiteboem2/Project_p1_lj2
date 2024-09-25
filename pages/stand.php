98c560e04149f8c8cf21c7dfdaf7122cd26b17d9
mahdi2662 <madialkhalidi83@gmail.com>
2024-09-25 10:26:47 +0200

    stands erbij

--- pages/stand.php
+++ pages/stand.php
@@ -1,15 +1,45 @@
 <?php include_once "../includes/header.php" ?>
-<!-- All extra links for styling etc.-->
-<title>Sneakerness - </title>
+<link rel="stylesheet" href="../assest/css/col.css">
+<title>Sneakerness - Stand Verkoop</title>
 </head>
 
 <body>
     <?php include_once "../includes/navbar.php" ?>
 
- 
-    <h2></h2>
-    <h3></h3>
-    <p></p>
+    <section class="stand-verkoop col-12">
+        <div class="container">
+            <div class="stand-intro">
+                <h2>Verkoop je Sneaker Stand</h2>
+                <p>Maak deel uit van het grootste sneaker evenement. Verkoop je unieke collectie in een professionele stand!</p>
+            </div>
+
+            <div class="stand-cards row">
+                <div class="stand-card col-4 ">
+                    <img class="stand-img sneakerJPG" src="../img/BlueNIke.png" alt="Stand Type 1">
+                    <h3>Stand</h3>
+                    <p>Perfect voor kleinere collecties.</p>
+                    <span class="price">€250</span>
+                    <button class="btn">Boek Nu</button>
+                </div>
+
+                <div class="stand-card col-4">
+                    <img src="img/stand2.jpg" alt="Stand Type 2">
+                    <h3>Stand</h3>
+                    <p>Voor de middelgrote collecties.</p>
+                    <span class="price">€500</span>
+                    <button class="btn">Boek Nu</button>
+                </div>
+
+                <div class="stand-card col-4 ">
+                    <img src="img/stand3.jpg" alt="Stand Type 3">
+                    <h3>Stand</h3>
+                    <p>Ideaal voor grotere collecties en merken.</p>
+                    <span class="price">€1000</span>
+                    <button class="btn">Boek Nu</button>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <?php include_once "../includes/footer.php" ?>
 
