afa867e3b71cf887e1c6551f965296c2b567625a
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-09-27 10:16:26 +0200

    begin aan stands pagina

--- pages/stand.php
+++ pages/stand.php
@@ -9,8 +9,8 @@
     <section class="stand-verkoop col">
         <div class="container">
             <div class="stand-intro">
-                <h2>Huur u sneaker stand!</h2>
-                <p>Maak deel uit van het grootste sneaker evenement. Verkoop je unieke collectie in een professionele stand!</p>
+                <h2>Huur uw sneaker stand!</h2>
+                <p>Verkoop je producten en diensten tijdens het event door een stand te huren. Of je nu een partner, shop of private seller bent, met onze verschillende standopties zorgen wij voor de perfecte plek om jouw merk in de schijnwerpers te zetten. Kies uit verschillende standcategorieën en bereik honderden (of duizenden) bezoekers tijdens dit unieke tweedaagse evenement!</p>
             </div>
 
             <div class="stand-cards row">
@@ -18,25 +18,25 @@
                     
                     <img class="stand-img  " src="../img/standAA+.png" alt="Stand Type 1">
                     
-                    <h3>Stand</h3>
-                    <p>AA+</p>
-                    <span class="price">€250</span>
+                    <h3 class="cardH3" >L-Stand(deluxe)</h3>
+                    <p class="cardP" ><span>AA+</span><br> Locatie: Deze stand bevindt zich op een prominente plaats, dicht bij de ingang van het event. Het is de eerste stand die bezoekers tegenkomen, waardoor je meteen maximale aandacht krijgt.</p>
+                    <span class="price">€300</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card col-4">
                 <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
-                    <h3>Stand</h3>
-                    <p>AA</p>
-                    <span class="price">€500</span>
+                <h3 class="cardH3">L-Stand</h3>
+                    <p class="cardP" ><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
+                    <span class="price">€200</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card col   ">
                 <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
-                    <h3>Stand</h3>
-                    <p>A</p>
-                    <span class="price">€1000</span>
+                <h3 class="cardH3">I-Stand</h3>
+                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement, maar nog steeds op een strategische plek met gematigd voetverkeer.</p>
+                    <span class="price">€150</span>
                     <button class="btn">Boek Nu</button>
                 </div>
             </div>
