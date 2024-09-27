abee7bf96f568b063a6768c367c21999c33a7fc6
Alperen <alperenkenan40@gmail.com>
2024-09-27 11:06:51 +0200

    Special Status pagina, bugs fixed

--- pages/stand.php
+++ pages/stand.php
@@ -10,61 +10,40 @@
     <section class="stand-verkoop col">
         <div class="container">
             <div class="stand-intro">
-<<<<<<< HEAD
-                <h2>Huur u sneaker stand!</h2>
-                <p>Maak deel uit van het grootste sneaker evenement. Verkoop je unieke collectie in een professionele
-                    stand!</p>
-=======
                 <h2>Huur uw sneaker stand!</h2>
-                <p>Verkoop je producten en diensten tijdens het event door een stand te huren. Of je nu een partner, shop of private seller bent, met onze verschillende standopties zorgen wij voor de perfecte plek om jouw merk in de schijnwerpers te zetten. Kies uit verschillende standcategorieën en bereik honderden (of duizenden) bezoekers tijdens dit unieke tweedaagse evenement!</p>
->>>>>>> e8e8459a13b9b5173f1a2fd39d6c477414544289
+                <p>Verkoop je producten en diensten tijdens het event door een stand te huren. Of je nu een partner,
+                    shop of private seller bent, met onze verschillende standopties zorgen wij voor de perfecte plek om
+                    jouw merk in de schijnwerpers te zetten. Kies uit verschillende standcategorieën en bereik honderden
+                    (of duizenden) bezoekers tijdens dit unieke tweedaagse evenement!</p>
             </div>
 
             <div class="stand-cards row">
                 <div class="stand-card col-4 ">
 
                     <img class="stand-img  " src="../img/standAA+.png" alt="Stand Type 1">
-<<<<<<< HEAD
 
-                    <h3>Stand</h3>
-                    <p>AA+</p>
-                    <span class="price">€250</span>
-=======
-                    
-                    <h3 class="cardH3" >L-Stand(deluxe)</h3>
-                    <p class="cardP" ><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de eerste stand die bezoekers tegenkomen, waardoor je  aandacht krijgt.</p>
+                    <h3 class="cardH3">L-Stand(deluxe)</h3>
+                    <p class="cardP"><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de
+                        eerste stand die bezoekers tegenkomen, waardoor je aandacht krijgt.</p>
                     <span class="price">€300</span>
->>>>>>> e8e8459a13b9b5173f1a2fd39d6c477414544289
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card col-4">
-<<<<<<< HEAD
                     <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
-                    <h3>Stand</h3>
-                    <p>AA</p>
-                    <span class="price">€500</span>
-=======
-                <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
-                <h3 class="cardH3">L-Stand</h3>
-                    <p class="cardP" ><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
+                    <h3 class="cardH3">L-Stand</h3>
+                    <p class="cardP"><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van
+                        het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
                     <span class="price">€200</span>
->>>>>>> e8e8459a13b9b5173f1a2fd39d6c477414544289
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card col   ">
-<<<<<<< HEAD
                     <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
-                    <h3>Stand</h3>
-                    <p>A</p>
-                    <span class="price">€1000</span>
-=======
-                <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
-                <h3 class="cardH3">I-Stand</h3>
-                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement, maar nog steeds op een plek met gematigd voetverkeer.</p>
+                    <h3 class="cardH3">I-Stand</h3>
+                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement,
+                        maar nog steeds op een plek met gematigd voetverkeer.</p>
                     <span class="price">€150</span>
->>>>>>> e8e8459a13b9b5173f1a2fd39d6c477414544289
                     <button class="btn">Boek Nu</button>
                 </div>
             </div>
@@ -74,68 +53,79 @@
     echo '<div style="border-top: 2px solid black; width: 100%; margin: 20px 0;"></div>';
     ?>
 
-<section class="stand-verkoop col">
+    <section class="stand-verkoop col">
         <div class="container">
             <div class="stand-intro">
                 <h2>Huur uw side-stand stand!</h2>
-                <p>Ben je klaar om je merk écht in de schijnwerpers te zetten? Naast de populairste sneakerverkopers vind je op ons event ook een scala aan unieke side-stands. Denk aan smakelijke foodtrucks, een te gekke kids corner, de beste customizers, tattoo- en barbershops, én live DJ-sets die de sfeer naar een hoger niveau tillen. Dit is dé kans om jouw producten of diensten direct onder de aandacht te brengen van duizenden enthousiaste bezoekers. Huur nu je eigen stand en maak deel uit van dit onvergetelijke tweedaagse evenement – waar jouw merk de show steelt!</p>
+                <p>Ben je klaar om je merk écht in de schijnwerpers te zetten? Naast de populairste sneakerverkopers
+                    vind je op ons event ook een scala aan unieke side-stands. Denk aan smakelijke foodtrucks, een te
+                    gekke kids corner, de beste customizers, tattoo- en barbershops, én live DJ-sets die de sfeer naar
+                    een hoger niveau tillen. Dit is dé kans om jouw producten of diensten direct onder de aandacht te
+                    brengen van duizenden enthousiaste bezoekers. Huur nu je eigen stand en maak deel uit van dit
+                    onvergetelijke tweedaagse evenement – waar jouw merk de show steelt!</p>
             </div>
 
             <div class="stand-cards2 row">
                 <div class="stand-card2 col-4 ">
-                    
+
                     <img class="stand-img  " src="../img/standAA+.png" alt="Stand Type 1">
-                    
-                    <h3 class="cardH3" >L-Stand(deluxe)</h3>
-                    <p class="cardP" ><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de eerste stand die bezoekers tegenkomen, waardoor je  aandacht krijgt.</p>
+
+                    <h3 class="cardH3">L-Stand(deluxe)</h3>
+                    <p class="cardP"><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de
+                        eerste stand die bezoekers tegenkomen, waardoor je aandacht krijgt.</p>
                     <span class="price">€300</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col-4">
-                <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
-                <h3 class="cardH3">L-Stand</h3>
-                    <p class="cardP" ><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
+                    <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
+                    <h3 class="cardH3">L-Stand</h3>
+                    <p class="cardP"><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van
+                        het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
                     <span class="price">€200</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col   ">
-                <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
-                <h3 class="cardH3">I-Stand</h3>
-                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement, maar nog steeds op een plek met gematigd voetverkeer.</p>
+                    <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
+                    <h3 class="cardH3">I-Stand</h3>
+                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement,
+                        maar nog steeds op een plek met gematigd voetverkeer.</p>
                     <span class="price">€150</span>
                     <button class="btn">Boek Nu</button>
-                    
+
                 </div>
             </div>
             <div class="stand-cards2 row">
                 <div class="stand-card2 col-4 ">
-                    
+
                     <img class="stand-img  " src="../img/standAA+.png" alt="Stand Type 1">
-                    
-                    <h3 class="cardH3" >L-Stand(deluxe)</h3>
-                    <p class="cardP" ><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de eerste stand die bezoekers tegenkomen, waardoor je  aandacht krijgt.</p>
+
+                    <h3 class="cardH3">L-Stand(deluxe)</h3>
+                    <p class="cardP"><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de
+                        eerste stand die bezoekers tegenkomen, waardoor je aandacht krijgt.</p>
                     <span class="price">€300</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col-4">
-                <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
-                <h3 class="cardH3">L-Stand</h3>
-                    <p class="cardP" ><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
+                    <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
+                    <h3 class="cardH3">L-Stand</h3>
+                    <p class="cardP"><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van
+                        het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
                     <span class="price">€200</span>
                     <button class="btn">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col   ">
-                <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
-                <h3 class="cardH3">I-Stand</h3>
-                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement, maar nog steeds op een plek met gematigd voetverkeer.</p>
+                    <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
+                    <h3 class="cardH3">I-Stand</h3>
+                    <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement,
+                        maar nog steeds op een plek met gematigd voetverkeer.</p>
                     <span class="price">€150</span>
                     <button class="btn">Boek Nu</button>
                 </div>
-        </div>
+            </div>
     </section>
 
     <?php include_once "../includes/footer.php" ?>
