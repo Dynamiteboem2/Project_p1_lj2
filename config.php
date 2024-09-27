e913f8627b94afc21c27e9a54e08bcbb6978f195
Alperen <alperenkenan40@gmail.com>
2024-09-27 11:05:52 +0200

    Special Status pagina, bugs fixed

--- config.php
+++ config.php
@@ -1,7 +1,3 @@
 <?php
 define("ROOT", __DIR__ . "/");
-<<<<<<< HEAD
 define("URL", "http://projectsne.com");
-=======
-define("URL", "http://projectsneakerness.com");
->>>>>>> e8e8459a13b9b5173f1a2fd39d6c477414544289
