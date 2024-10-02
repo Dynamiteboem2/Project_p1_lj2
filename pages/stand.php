5917d9a6f4d1c027488aa8cc5d7454798c2b6346
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-02 11:02:22 +0200

    popups erbij

--- pages/stand.php
+++ pages/stand.php
@@ -19,32 +19,30 @@
 
             <div class="stand-cards row">
                 <div class="stand-card col-4 ">
-
-                    <img class="stand-img  " src="../img/standAA+.png" alt="Stand Type 1">
-
+                    <img class="stand-img" src="../img/standAA+.png" alt="Stand Type 1">
                     <h3 class="cardH3">L-Stand(deluxe)</h3>
                     <p class="cardP"><span>AA+</span><br> Locatie: Deze stand is in de ingang van het event. Het is de
                         eerste stand die bezoekers tegenkomen, waardoor je aandacht krijgt.</p>
                     <span class="price">€300</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('L-Stand(deluxe)', '€300')">Boek Nu</button>
                 </div>
 
                 <div class="stand-card col-4">
-                    <img class="stand-img  " src="../img/standAA.png" alt="Stand Type 1">
+                    <img class="stand-img" src="../img/standAA.png" alt="Stand Type 1">
                     <h3 class="cardH3">L-Stand</h3>
                     <p class="cardP"><span>AA</span><br> Locatie: Deze stand is centraal geplaatst, in het midden van
                         het event, waardoor er altijd veel voetverkeer is van alle richtingen.</p>
                     <span class="price">€200</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('L-Stand', '€200')">Boek Nu</button>
                 </div>
 
-                <div class="stand-card col   ">
-                    <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
+                <div class="stand-card col">
+                    <img class="stand-img" src="../img/standA.png" alt="Stand Type 1">
                     <h3 class="cardH3">I-Stand</h3>
                     <p class="cardP"><span>A</span><br> Locatie: Deze stand bevindt zich aan de rand van het evenement,
                         maar nog steeds op een plek met gematigd voetverkeer.</p>
                     <span class="price">€150</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('I-Stand', '€150')">Boek Nu</button>
                 </div>
             </div>
         </div>
@@ -67,65 +65,98 @@
 
             <div class="stand-cards2 row">
                 <div class="stand-card2 col-4 ">
-                    
-                    <img class="stand-img  " src="../img/food_and_drink1.jpg" alt="Stand Type 1">
-                    
-                    <h3 class="cardH3" >eten en drinken</h3>
-                    <p class="cardP" >Locatie: Deze stand bevindt zich centraal geplaats. In het midden van het event, waardoor er veel mensen zijn.</p>
+                    <img class="stand-img" src="../img/food_and_drink1.jpg" alt="Stand Type 1">
+                    <h3 class="cardH3">eten en drinken</h3>
+                    <p class="cardP">Locatie: Deze stand bevindt zich centraal geplaats. In het midden van het event, waardoor er veel mensen zijn.</p>
                     <span class="price">€150</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('eten en drinken', '€150')">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col-4">
-                <img class="stand-img  " src="../img/kids-corner1.jpg" alt="Stand Type 1">
-                <h3 class="cardH3">kids corner</h3>
-                    <p class="cardP" >Locatie: Deze stand bevindt zich aan de rechterkant van het event,alsnog een plek waar  enorm veel mensen komen.</p>
+                    <img class="stand-img" src="../img/kids-corner1.jpg" alt="Stand Type 1">
+                    <h3 class="cardH3">kids corner</h3>
+                    <p class="cardP">Locatie: Deze stand bevindt zich aan de rechterkant van het event,alsnog een plek waar  enorm veel mensen komen.</p>
                     <span class="price">€87,50</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('kids corner', '€87,50')">Boek Nu</button>
                 </div>
 
-                <div class="stand-card2 col   ">
-                <img class="stand-img  " src="../img/custemise1.jpg" alt="Stand Type 1">
-                <h3 class="cardH3">Customizers-area</h3>
+                <div class="stand-card2 col">
+                    <img class="stand-img" src="../img/custemise1.jpg" alt="Stand Type 1">
+                    <h3 class="cardH3">Customizers-area</h3>
                     <p class="cardP">Locatie: Deze stand bevindt zich links in het midden, een prima plek waar genoeg mensen langlopen.</p>
                     <span class="price">€100</span>
-                    <button class="btn">Boek Nu</button>
-
+                    <button class="btn" onclick="openPopup('Customizers-area', '€100')">Boek Nu</button>
                 </div>
             </div>
+
             <div class="stand-cards2 row">
                 <div class="stand-card2 col-4 ">
-
-                    <img class="stand-img  " src="../img/tatoo1.jpg" alt="Stand Type 1">
-                    
-                    <h3 class="cardH3" >tattoo- en barbershop</h3>
-                    <p class="cardP" >Locatie: Deze stand bevind zich in de linkerhoek van het event, er komen nogsteeds veel mensen daar kijken.</p>
+                    <img class="stand-img" src="../img/tatoo1.jpg" alt="Stand Type 1">
+                    <h3 class="cardH3">tattoo- en barbershop</h3>
+                    <p class="cardP">Locatie: Deze stand bevind zich in de linkerhoek van het event, er komen nogsteeds veel mensen daar kijken.</p>
                     <span class="price">€90</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('tattoo- en barbershop', '€90')">Boek Nu</button>
                 </div>
 
                 <div class="stand-card2 col-4">
-                <img class="stand-img  " src="../img/DJ-Set1.jpg" alt="Stand Type 1">
-                <h3 class="cardH3">DJ-sets</h3>
-                    <p class="cardP" >Locatie: Deze stand bevindt zich rechtsboven op het event, het is daar vaak druk daar ook andere activiteiten.</p>
+                    <img class="stand-img" src="../img/DJ-Set1.jpg" alt="Stand Type 1">
+                    <h3 class="cardH3">DJ-sets</h3>
+                    <p class="cardP">Locatie: Deze stand bevindt zich rechtsboven op het event, het is daar vaak druk daar ook andere activiteiten.</p>
                     <span class="price">€110</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('DJ-sets', '€110')">Boek Nu</button>
                 </div>
 
-                <div class="stand-card2 col   ">
-                    <img class="stand-img  " src="../img/standA.png" alt="Stand Type 1">
+                <div class="stand-card2 col">
+                    <img class="stand-img" src="../img/standA.png" alt="Stand Type 1">
                     <h3 class="cardH3">I-Stand</h3>
                     <p class="cardP">Locatie: Deze stand bevindt zich aan de rand van het evenement,
                         maar nog steeds op een plek met gematigd voetverkeer.</p>
                     <span class="price">€150</span>
-                    <button class="btn">Boek Nu</button>
+                    <button class="btn" onclick="openPopup('I-Stand', '€150')">Boek Nu</button>
                 </div>
             </div>
+        </div>
     </section>
 
     <?php include_once "../includes/footer.php" ?>
 
-    <!-- All extra scripts -->
-</body>
+    <!-- Pop-up HTML -->
+    <div id="overlay" style="display: none;"></div>
+    <div id="popup" style="display: none;">
+    <h2>Boeking Bevestigen</h2>
+        <p id="popup-text"></p>
+        <button id="closePopup">Sluiten</button>
+    </div>
 
+    <script src="../js/popup.js"></script>
+    
+    <style>
+        #overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.7);
+            display: none;
+            z-index: 1000;
+        }
+
+        #popup {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: white;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            z-index: 1001;
+            display: none;
+        }
+
+        #closePopup {
+            margin-top: 10px;
+        }
+    </style>
+</body>
 </html>
