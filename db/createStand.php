5809d925901befae2a46a579aca945175dc5c0dd
Dennis Oostrom <336914@student.mboutrecht.nl>
2024-10-10 12:45:17 +0200

    form changes stand page

--- db/createStand.php
+++ db/createStand.php
@@ -3,7 +3,7 @@
 include_once("conn.php");
 
 $first_name = $_POST['first-name'];
-$last_name = $_POST['infix-name'] . $_POST['last-name'];
+$last_name = $_POST['infix-name'] . " " . $_POST['last-name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $birthdate = $_POST['birthdate'];
@@ -14,10 +14,10 @@ $sql = "INSERT INTO stand (firstName, lastName, email, phoneNumber, birthdate, s
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
     // Succesfuly buy a stand
-    header("Location: ../pages/stand.php?message=Stand bought successfully");
+    header("Location: ../pages/stand.php?message=Stand succesvol gehuurt!");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
-    header("Location: ../pages/stand.php?error=Error buying stand");
+    header("Location: ../pages/stand.php?error=Er is een fout opgetreden tijdens het proberen te huren van een stand.");
 }
 
 $conn->close();
