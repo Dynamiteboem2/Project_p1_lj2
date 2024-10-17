4854ca2d22986a92c3c5823a3d023da930fa6469
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-17 16:23:49 +0200

    goed getest

--- pages/stand.php
+++ pages/stand.php
@@ -203,48 +203,7 @@
 
 
 
-<script>
-document.getElementById('payment-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
 
-    const formData = new FormData(this);
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', this.action, true);
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            document.querySelectorAll('.error-message').forEach(div => div.innerHTML = '');
-
-            if (response.success === false) {
-                for (const [key, message] of Object.entries(response.errors)) {
-                    const errorDiv = document.getElementById('error-' + key);
-                    if (errorDiv) {
-                        errorDiv.innerHTML = message; 
-                    }
-                }
-            } else {
-                window.location.href = "../pages/stand.php?message=Stand succesvol gehuurt!";
-            }
-        } else {
-            alert("Er is een fout opgetreden bij het verzenden van de gegevens.");
-        }
-    };
-
-    xhr.send(formData);
-});
-
-// error message gaat weg
-document.querySelectorAll('#payment-form input, #payment-form select').forEach(input => {
-    input.addEventListener('input', function() {
-        const errorDiv = document.getElementById('error-' + this.name);
-        if (errorDiv) {
-            errorDiv.innerHTML = ''; 
-        }
-    });
-});
-
-</script>
 
 
 
@@ -328,9 +287,9 @@ document.querySelectorAll('#payment-form input, #payment-form select').forEach(i
             color: #721c24;
         }
     </style>
+</div>
 
-
-     <script src="..js/validation.js"></script>
+     <script src="../js/validation.js"></script>
     <script src="../js/scrollreveal"></script>
     <script src="../js/popup.js"></script>
     <?php include_once "../includes/footer.php" ?>
