2f2cd49c5f9c80403576b952ee47bffcbe326110
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-17 15:23:51 +0200

    inlog gedaan

--- pages/inlog.php
+++ pages/inlog.php
@@ -1,11 +1,9 @@
 <?php include_once "../includes/header.php" ?>
-inlog
-<title>Sneakerness - </title>
+<link rel="stylesheet" href="../assets/css/inlog.css">
+<title>Sneakerness</title>
 </head>
-
 <body>
     <?php include_once "../includes/navbar.php" ?>
-
     <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,15 +11,49 @@ inlog
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .sidebar {
+            width: 200px; /* Define width of the sidebar */
+            background-color: black;
+            float: left; /* Sidebar positioned to the left */
+            padding: 1rem; /* Add some padding for spacing */
+            color: white;
+        }
+
+        .main {
+            margin-left: 220px; /* Add left margin to make space for the sidebar */
+            padding: 1rem; /* Add some padding for spacing */
+        }
+
+        /* Add styles for the profile form */
+        .form-group {
+            margin-bottom: 1rem;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 0.5rem;
+        }
+
+        input {
+            width: 100%;
+            padding: 0.5rem;
+            margin-bottom: 1rem;
+        }
+
+        h1 {
+            color: #333;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="sidebar">
             <ul>
-                <li><a href="#"><i class="fas fa-user"></i></a></li>
-                <li><a href="#"><i class="fas fa-lock"></i></a></li>
-                <li><a href="#"><i class="fas fa-bell"></i></a></li>
-                <li><a href="#"><i class="fas fa-sync-alt"></i></a></li>
+                <li><button><a href="#"><i class="fas fa-user"></i></a></button></li>
+                <li><button><a href="#"><i class="fas fa-lock"></i></a></button></li>
+                <li><button><a href="#"><i class="fas fa-shopping-cart"></i></a></button></li>
+                <li><button><a href="#"><i class="fas fa-trash"></i></a></button></li>
             </ul>
         </div>
         <div class="main">
@@ -47,10 +79,9 @@ inlog
                     <label for="phone">Phone:</label>
                     <input type="tel" id="phone" name="phone">
                 </div>
-
+            </form>
+        </div>
+    </div>
     <?php include_once "../includes/footer.php" ?>
-
-    <!-- All extra scripts -->
 </body>
-
 </html>
