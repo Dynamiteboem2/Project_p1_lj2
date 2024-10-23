1e3a5a3473f632f2c8fa9fb8ee92b24e6d24d91d
mahdi2662 <madialkhalidi83@gmail.com>
2024-10-23 11:02:38 +0200

    transion proberen maken

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,14 @@
+
 <header>
     <div class="nav">
         <nav>
             <h2 class="logo"><a href="<?php echo URL . "/pages/homepage.php"; ?>">SNEAKERNESS</a></h2>
             <ul>
-                <li><a href="<?php echo URL . "/pages/stand.php" ?>">STANDS</a></li>
-                <li><a href="/pages/Tickets.php">TICKETS KOPEN</a></li>
+                <li><a href="<?php echo URL . "/pages/stand.php"; ?>">STANDS</a></li>
+                <li><a href="<?php echo URL . "/pages/Tickets.php"; ?>">TICKETS KOPEN</a></li>
                 <li><a href="<?php echo URL . "/pages/faq.php"; ?>">FAQ</a></li>
                 <li><a href="<?php echo URL . "/pages/contact.php"; ?>">CONTACT</a></li>
-                <li><a href="/pages/gebruiker_overzicht/profiel_overzicht.php">test</a></li>
+                <li><a href="<?php echo URL . "/pages/gebruiker_overzicht/profiel_overzicht.php"; ?>">test</a></li>
             </ul>
 
             <!-- Check inlogged or not -->
